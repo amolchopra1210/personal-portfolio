84d66e9e8c4e06aefffc683ef8a8035af1f564c2
Amol <amolviper@gmail.com>
2022-04-09 21:16:32 +0530

    added work project react-dashboard

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Work1 from "../assets/work1.png";
 import Work2 from "../assets/work2.png";
 import Work3 from "../assets/work3.png";
+import Work4 from "../assets/work4.png";
 
 const Work = () => {
   return (
@@ -77,6 +78,41 @@ const Work = () => {
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     <a
                       href="https://github.com/amolchopra1210/typescript-quiz-trivia"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Code
+                    </a>
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Work4})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 text-center group-hover:opacity-100">
+              <span className="text-2xl mx-auto font-bold text-white tracking-wider">
+                React-Typescript Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    <a
+                      href="https://react-admin-dashboard-amolchopra1210.vercel.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Link
+                    </a>
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    <a
+                      href="https://github.com/amolchopra1210/react-admin-dashboard"
                       target="_blank"
                       rel="noreferrer"
                     >
