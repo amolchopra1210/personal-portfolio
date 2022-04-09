d89e93d06f45ef18da026da7ee247420f78d141f
Amol <amolviper@gmail.com>
2022-04-09 21:29:30 +0530

    added work project react-dashboard

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 pt-[100px] pb-[100px]"
+      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
     >
       <form
         method="POST"
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,7 +6,7 @@ import Work4 from "../assets/work4.png";
 
 const Work = () => {
   return (
-    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f] pb-[100px]">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
